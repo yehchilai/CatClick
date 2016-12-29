5953b1fca60db6f6c192df77424290e927fb5a56
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-12-28 16:43:13 -0800

    update - Project Requirements II
    - the cat's name
    - a picture of the cat
    - text showing the number of clicks

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 // console.log("javascript");
 var clicks = 0;
+var catName1 = "Catty";
+var catName2 = "Kitty";
 
 jQuery(document).ready(function ($) { //fire on DOM ready
-  $('#image').click(function(){
+
+  $('#catName1').text(catName1);
+  $('#catName2').text(catName2);
+
+  $('#image1').click(function(){
+    clickIncrese();
+  });
+
+  $('#image2').click(function(){
+    clickIncrese();
+  });
+})
+
+
+var clickIncrese = function(){
   clicks++;
   $('#clickNumber').text(clicks);
-  console.log(clicks);
-});
-})
+  // console.log(clicks);
+};
