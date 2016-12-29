6034464c73b1d0770a0dcb3716ab9f01513276f4
Yeh-chi Lai <yehchilai@csupomona.edu>
2016-12-28 18:24:58 -0800

    update - Project Requirements for Cat Clicker Premium
    -The application should display
    1. a list of at least 5 cats, listed by name
    2. an area to display the selected cat
    - In the cat display area, the following should be displayed
    1. the cat's name
    2. a picture of the cat
    3. text showing the number of clicks

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 // console.log("javascript");
-var clicks = 0;
-var catName1 = "Catty";
-var catName2 = "Kitty";
+var clicks = [0,0,0,0,0];
+var catNames = ["Catty", "Kitty", "Citty", "Kit", "Cat"];
+var catImages = ["cat.jpg", "cat2.jpg", "cat3.jpg", "cat4.jpeg", "cat5.jpeg"];
 
 jQuery(document).ready(function ($) { //fire on DOM ready
 
-  $('#catName1').text(catName1);
-  $('#catName2').text(catName2);
+  var list = $('#list');
+  // main.after('<div class="col-md-3">');
+  for(var i = 0; i < catNames.length; i++){
+    var name = catNames[i];
+    var cNumber = clicks[i];
+    var cImage = catImages[i];
+    var elem = '<li id=\''+name+'\'>' + name + '</li>';
+    list.append(elem);
+    var elemClick = $('#'+name);
+    elemClick.click((function(catName, clickNumber, catImage, index){
 
-  $('#image1').click(function(){
-    clickIncrese();
-  });
+      return function(){
+        // current number of the image
+        $('#click').empty();
+        $('#click').text(clicks[index]);
+        // console.log(test);
+        $('#image').empty();
+        $('#image').append('<img id="Click'+catName+'" src="'+catImage+'">');
+        $('#Click'+catName).click((function(imgageIndex){
+          return function(){
+            console.log(imgageIndex);
+            clicks[imgageIndex]++;
+            $('#click').empty();
+            $('#click').text(clicks[index]);
+          }
+        })(index));
+      };
 
-  $('#image2').click(function(){
-    clickIncrese();
-  });
+    })(name, cNumber, cImage, i));
+  }
+  // main.after('</div>');
+  // main.after('<div class="col-md-4"><h3>Click Number:</h3><h2 id="click"><h2></div>');
+  // main.after('<div id="image" class="col-md-5"></div>');
 })
 
 
